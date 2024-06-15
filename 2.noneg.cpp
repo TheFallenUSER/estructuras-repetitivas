13b0dc7d07563342f9cd3efbdec7c85123b0fbf1
TheFallenUSER <eduardopea616@gmail.com>
2024-06-15 00:12:27 -0500

    Añadido ejercicio 2 que lee números hasta que se introduce un valor negativo

---
+++ 2.noneg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num;
+	cout<<"Introduce un numero:"<<endl;
+	cin>>num;
+	do{
+		cout<<"Introduce otro numero:"<<endl;
+		cin>>num;
+	}while(num>=0);
+	return 0;
+}
+	
+	
