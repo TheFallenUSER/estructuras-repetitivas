680ffd1932fdbd3292412ea832bb938cd47c6f65
TheFallenUSER <eduardopea616@gmail.com>
2024-06-14 21:39:45 -0500

    Añadida validación para que la cantidad de múltiplos a mostrar sea mayor a 0.

--- 1.mult7.cpp
+++ 1.mult7.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main(){
 	int n,mul;
-	cout<<"Ingresa la cantidad de multiplos de 7:"<<endl;
-	cin>>n;
+	do{
+		cout<<"Ingresa la cantidad de multiplos de 7:"<<endl;
+		cin>>n;
+		if(n<=0){
+			cout<<"ERROR: La cantidad de multiplos no puede ser menor a 1. \n"<<endl; //El carácter "\n" indica salto de línea
+		}
+	}while(n<=0);
 	int i=1;
 	while(i<=n){
 		mul=i*7;
